30f4321921d414ad76d0e14d41dfd7aebf498cd2
lpcorrea <luiz.pedro@hotmail.com>
2019-06-06 19:49:00 -0300

    Update AnalisaLexico

--- src/unesc/analisador/AnalisadorLexico.java
+++ src/unesc/analisador/AnalisadorLexico.java
@@ -6,9 +6,33 @@ import java.util.Stack;
 public class AnalisadorLexico {
     
     public Stack<String> gerarTokens(String programa){
-        String[] tokens = programa.split(" ");
+        //Criação das Pilhas        
         Stack<String> pilhaTokens = new Stack<>();
-        //TODO: Adicione os elementos de "programa" dentro de "pilhaTokens". A ordem de leitura de pilhaToken deve ser a mesma contida na variável "programa"
+        Stack<String> pilhaAux = new Stack<>();
+             
+        int a = -1;
+        /*Vetor "tokens" receberá oque estiver sendo passado 
+        por parametro na varivel "texto". */
+         
+        //Vetor "tokens", cereberá o conteudo quebro por espaços em branco        
+        String[] tokens = programa.split(" ");
+        //Remove os espaços em branco e Verifica se "s" está vazio.
+        //Joga o texto formatado para a pilha Auxiliar.
+        for(String s : tokens) {
+            if(!s.trim().isEmpty()){
+                pilhaAux.push(s);
+                a++;
+            }
+        }
+        
+        //Joga a pilha invertida para a pilha"pilhaIni"        
+        while(!pilhaAux.isEmpty()){
+            for(int i=0;i<=a;i++){
+                pilhaTokens.push(pilhaAux.pop());
+                      
+            }
+            
+        }
         return pilhaTokens;
     }
     
